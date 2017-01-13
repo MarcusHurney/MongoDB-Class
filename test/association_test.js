155c020ab3bea89af51cf7aca389449d1acc2498
Marcus Hurney <marcushurney@Marcuss-MacBook-Pro.local>
2017-01-13 18:17:52 -0500

    Adds populate examples

--- test/association_test.js
+++ test/association_test.js
@@ -4,34 +4,50 @@ const User = require('../src/user');
 const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
 
-describe('Associations', () => {
-  // lowercase declarations indicate an instance of the model, not the model itself
+describe('Assocations', () => {
   let joe, blogPost, comment;
-  beforeEach(done => {
+
+  beforeEach((done) => {
     joe = new User({ name: 'Joe' });
     blogPost = new BlogPost({ title: 'JS is Great', content: 'Yep it really is' });
-    comment = new Comment({ content: 'Congrats on your big post' });
+    comment = new Comment({ content: 'Congrats on great post' });
 
-    // associate the blogPost with joe
-    // even though in user.js blogPosts is of the type 'Schema.Types.ObjectId'
-    // mongoose knows how to make the association if the whole instance of blogPost is pushed
-    // same on blogPost and its comments and comment's user prop
     joe.blogPosts.push(blogPost);
-    // add comment to blogPost
-    blogPost.comment.push(comment);
-    // add user to comment
+    blogPost.comments.push(comment);
     comment.user = joe;
 
-    // save all three instances, then call done()
     Promise.all([joe.save(), blogPost.save(), comment.save()])
       .then(() => done());
   });
 
-  it('saves a relation between a user and a blogpost', done => {
+  it('saves a relation between a user and a blogpost', (done) => {
     User.findOne({ name: 'Joe' })
       .populate('blogPosts')
-      .then(user => {
+      .then((user) => {
         assert(user.blogPosts[0].title === 'JS is Great');
+        done();
+      });
+  });
+
+  it('saves a full relation graph', (done) => {
+    User.findOne({ name: 'Joe' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'
+          }
+        }
+      })
+      .then((user) => {
+        assert(user.name === 'Joe');
+        assert(user.blogPosts[0].title === 'JS is Great');
+        assert(user.blogPosts[0].comments[0].content === 'Congrats on great post');
+        assert(user.blogPosts[0].comments[0].user.name === 'Joe');
+
         done();
       });
   });
